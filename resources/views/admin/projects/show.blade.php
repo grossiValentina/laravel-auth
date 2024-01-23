48df546d9846b5565cb907c9fef78042bfae5c76
grossiValentina <grossivalentina435@gmail.com>
2024-01-23 19:10:28 +0100

    add: metodo show wip

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,7 +2,12 @@
 
 @section('content')
 <div class="container">
-    <h2 class="text-center pt-3">Progetto n. {{ $project->id }}</h2>
+    <h6 class="text-end pt-3">Progetto n. {{ $project->id }}</h6>
+    <p class="text-end">{{ $project->slug }}</p>
+</div>
+<div>
+    <h2 class="text-center p-3">{{ $project->titolo }}</h2>
+    <p class="text-center pt-3">{{ $project->descrizione }}</p>
 </div>
 
 @endsection
