4b6d336c13c0a66c8ff23e6f94ec6f2ff07d0363
grossiValentina <grossivalentina435@gmail.com>
2024-01-23 17:11:33 +0100

    add:creazione nuovo controller e  rotte ProjectController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
@@ -27,7 +28,7 @@ Route::middleware(['auth', 'verified'])
 
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-        
+        Route::resource('projects', ProjectController::class);
 
     });
 
