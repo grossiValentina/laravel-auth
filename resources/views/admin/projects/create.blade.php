cd429c031d408a3ea1009333cbb13a8a89ce0173
grossiValentina <grossivalentina435@gmail.com>
2024-01-23 19:28:45 +0100

    add: metodo create wip

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+@section('content')
+
+<div class="container">
+    <h2 class="text-center p-3">Crea un nuovo Progetto</h2>
+</div>
+
+@endsection
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,31 +2,36 @@
 
 @section('content')
     <div class="container">
-        <h2 class="text-center pt-3">Lista dei Progetti</h2>
-    </div>
 
-    <table class="table table-hover table-primary table-striped">
-        <thead>
-            <tr>
-                <th scope="col">id</th>
-                <th scope="col">Titolo</th>
-                <th scope="col">Descrizione</th>
-                <th scope="col">Selezione</th>
-            </tr>
-        </thead>
-        <tbody>
+        <div class="d-flex justify-content-between p-5">
+            <h2>Lista dei Progetti</h2>
+            <a class="btn btn-success text-center" href="{{ route('admin.projects.create') }}">Nuovo Progetto</a>
+        </div>
 
-            @foreach ($projects as $project)
+        <table class="table table-hover table-primary table-striped">
+            <thead>
                 <tr>
-                    <th scope="row">{{ $project->id }}</th>
-                    <td>{{ $project->titolo }}</td>
-                    <td>{{ $project->descrizione }}</td>
-                    <td>
-                        <a class="btn btn-success" href="{{ route('admin.projects.show', ['project' => $project->slug]) }}">Dettagli</a>
-                    </td>
+                    <th scope="col">id</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Selezione</th>
                 </tr>
-            @endforeach
+            </thead>
+            <tbody>
 
-        </tbody>
-    </table>
+                @foreach ($projects as $project)
+                    <tr>
+                        <th scope="row">{{ $project->id }}</th>
+                        <td>{{ $project->titolo }}</td>
+                        <td>{{ $project->descrizione }}</td>
+                        <td>
+                            <a class="btn btn-success"
+                                href="{{ route('admin.projects.show', ['project' => $project->slug]) }}">Dettagli</a>
+                        </td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
 @endsection
